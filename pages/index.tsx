539e912dd76cf5e476347cd9e6cfd4877b93a90a
Cesar Gomez <cesaregomezg0@gmail.com>
2021-05-22 11:23:32 -0500

    Primer commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Inicio = () => {
+    const texto: string = "Este es el inicio";
+    return (
+        <div>
+            <h1>{texto}</h1>
+            <p>Esto es un texto</p>
+        </div>
+    )
+}
+
+export default Inicio;
